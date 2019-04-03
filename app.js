b203e28b278f7b4e2ec0cb421f817957eeabd7ac
Wyatt Salmons <wyattsalmons@gmail.com>
2019-04-03 16:50:54 -0600

    completed $ref tutorial

--- app.js
+++ app.js
@@ -1,21 +1,13 @@
-Vue.component('greeting', {
-	template: '<p>Hey there, I am {{name}}. <button v-on:click="changeName">Change Name</button></p>',
-	data: function(){
-		return {
-			name: 'Yoshi'
-		}
+new Vue({
+	el: '#vue-app',
+	data: {
+		output: 'Your favorite food.'
 	},
 	methods: {
-		changeName: function(){
-			this.name = 'Mario';
+		readRefs: function(){
+			console.log(this.$refs.test.innerText);
+			this.output = this.$refs.input.value;
 		}
 	}
 });
 
-new Vue({
-	el: '#vue-app-one'
-});
-
-new Vue({
-	el: '#vue-app-two'
-});
