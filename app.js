4a755cf8b737f85995359e2a07e987d4d66a141a
Wyatt Salmons <wyattsalmons@gmail.com>
2019-04-02 21:23:42 -0600

    added basic files.

---
+++ app.js
@@ -0,0 +1,30 @@
+var one = new Vue({
+  el: '#vue-app-one',
+  data: {
+    title: 'Vue App One'
+  },
+  methods: {
+  },
+  computed: {
+    greet: function() {
+      return 'hello from app one';
+    }
+  }
+});
+
+var two = new Vue({
+  el: '#vue-app-two',
+  data: {
+    title: 'Vue App Two'
+  },
+  methods: {
+    changeTitle: function(){
+      one.title = two.title;
+    }
+  },
+  computed: {
+    greet: function() {
+      return 'hello from app two';
+    }
+  }
+});
