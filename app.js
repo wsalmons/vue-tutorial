27af9f2daece9a9a9675f36895cb006f230d2936
Wyatt Salmons <wyattsalmons@gmail.com>
2019-04-02 21:52:02 -0600

    reset for next tutorial

--- app.js
+++ app.js
@@ -1,30 +1,7 @@
 var one = new Vue({
-	el: '#vue-app-one',
-	data: {
-		title: 'Vue App One'
-	},
-	methods: {
-	},
-	computed: {
-		greet: function() {
-			return 'hello from app one';
-		}
-	}
+	el: '#vue-app-one'
 });
 
 var two = new Vue({
-	el: '#vue-app-two',
-	data: {
-		title: 'Vue App Two'
-	},
-	methods: {
-		changeTitle: function() {
-			one.title = two.title;
-		}
-	},
-	computed: {
-		greet: function() {
-			return 'hello from app two';
-		}
-	}
+	el: '#vue-app-two'
 });
