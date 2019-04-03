b5fae72793537d1592b21fe1694f86df0c9d80a9
Wyatt Salmons <wyattsalmons@gmail.com>
2019-04-02 22:26:29 -0600

    completed component tutorial

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-var one = new Vue({
+Vue.component('greeting', {
+	template: '<p>Hey there, I am {{name}}. <button v-on:click="changeName">Change Name</button></p>',
+	data: function(){
+		return {
+			name: 'Yoshi'
+		}
+	},
+	methods: {
+		changeName: function(){
+			this.name = 'Mario';
+		}
+	}
+});
+
+new Vue({
 	el: '#vue-app-one'
 });
 
-var two = new Vue({
+new Vue({
 	el: '#vue-app-two'
 });
